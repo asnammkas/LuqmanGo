f6055aa078ea336d6e69e8b4fa68e6a6493a89f5
asnammkas <asnamsanaf6600@gmail.com>
2026-04-12 07:38:55 +0530

    fix: update eslint flat config to support node globals in playwright.config

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.es2020,
+        ...globals.node,
       },
       parserOptions: {
         ecmaVersion: 'latest',
